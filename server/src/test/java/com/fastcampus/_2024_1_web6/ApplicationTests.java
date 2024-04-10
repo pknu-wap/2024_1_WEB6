7388480a3b148ede4e3d41e2bb3bed42593816d9
S <114375668+oneokiwa@users.noreply.github.com>
2024-04-10 18:44:22 +0900

    [chore] 인텔리제이 초기 프로젝트 생성
    백엔드 초기 세팅 완료

---
+++ server/src/test/java/com/fastcampus/_2024_1_web6/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus._2024_1_web6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
