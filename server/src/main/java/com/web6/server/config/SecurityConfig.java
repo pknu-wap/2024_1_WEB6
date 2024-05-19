c573df7ba88b49f2e0f9225d7772468edb619cf5
heejintv <144670992+heejintv@users.noreply.github.com>
2024-05-19 22:30:21 +0900

    최종

--- server/src/main/java/com/web6/server/config/SecurityConfig.java
+++ server/src/main/java/com/web6/server/config/SecurityConfig.java
@@ -1,96 +1,61 @@
 package com.web6.server.config;
 
-import com.web6.server.jwt.JwtAuthorizationFilter;
-
-import com.web6.server.oauth2login.service.CustomOAuth2UserService;
-import com.web6.server.oauth2login.HttpCookieOAuth2AuthorizationRequestRepository;
-import com.web6.server.oauth2login.handler.OAuth2AuthenticationFailureHandler;
-import com.web6.server.oauth2login.handler.OAuth2AuthenticationSuccessHandler;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
-    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
-    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
-    private final JwtAuthorizationFilter jwtAuthorizationFilter;
-
     @Bean
-    public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
-        return new HttpCookieOAuth2AuthorizationRequestRepository();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public BCryptPasswordEncoder bCryptPasswordEncoder() { //암호화메서드
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/login-page", "/api/members/login-page", "/loginError", "/sign-up", "/api/members/sign-up").anonymous()
+                        .requestMatchers("/", "/login", "/api/members/login","/sign-up", "/api/members/sign-up").permitAll()
+                        //.requestMatchers("/mypage/**").hasRole("USER") //USER라는 role만 접근허용
+                        .requestMatchers(("/hello")).permitAll()       //hello 경로에 대해 모든 사용자에게 접근 허용
                         .requestMatchers(("/testOpenAPI")).permitAll() //testOpenAPI 경로에 대해 모든 사용자에게 접근 허용
                         .requestMatchers(("/movie/findAll")).permitAll() //movie.findAll 경로에 대해 모든 사용자 접근 허용
-                        .anyRequest().authenticated()
+                        .anyRequest().authenticated() //로그인한 사용자만 허용
                 );
 
         http
                 .formLogin((auth) -> auth
-                        .loginPage("/login-page")
-                        .loginProcessingUrl("/api/members/login-page")
-                        .failureForwardUrl("/loginError")
-                        .defaultSuccessUrl("/", true)
+                        .loginPage("/login")
+                        .loginProcessingUrl("/api/members/login")
+                        .failureUrl("/sign-up") //회원가입 실패 시 리다이렉트할 URL 지정
+                        .permitAll()
                 );
 
-        http.httpBasic(AbstractHttpConfigurer::disable);
-
         http
-                .logout((auth) -> auth //일반 로그아웃
-                        .logoutUrl("/api/members/logout-page")
+                .logout((auth) -> auth
+                        .logoutUrl("/api/members/logout")
                         .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                        .deleteCookies("accessToken", "refreshToken")
-                        .deleteCookies("JSESSIONID")
                 );
 
         http
-                .csrf((auth) -> auth.disable());
+                .csrf((auth) -> auth.disable()); //추후 enable로 수정
 
         http
                 .sessionManagement((auth) -> auth
                         .maximumSessions(1)
-                        .maxSessionsPreventsLogin(true));
+                        .maxSessionsPreventsLogin(true)); //true면 초과시 새로운 로그인 차단, false면 초과시 기존 세션 하나 삭제
 
         http.sessionManagement((auth) -> auth
-                .sessionFixation().changeSessionId());
-
-        // 소셜 로그인
-
-        http.oauth2Login(configure ->
-                configure
-                        .authorizationEndpoint(config -> config.authorizationRequestRepository(httpCookieOAuth2AuthorizationRequestRepository))
-                        .userInfoEndpoint(config -> config.userService(customOAuth2UserService))
-                        .successHandler(oAuth2AuthenticationSuccessHandler)
-                        .failureHandler(oAuth2AuthenticationFailureHandler)
-        );
+                .sessionFixation().changeSessionId()); //세션 고정 보호
 
 
-        http.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
