670e78e88bc4e19e4a5524d8745cf396d935e39f
heejintv <144670992+heejintv@users.noreply.github.com>
2024-05-21 19:56:33 +0900

    [feature] 검색 기능 초기
     URL을 구성하여 요청을 보내면 (ex. /movie/search?actor=이병헌) 그와 관련된 영화가 나옴.
    추후 검색창을 만들고 검색 버튼을 눌렀을 때 영화 정보를 표시하게 할 예정임. 검색은 제목, 장르, 배우, 감독 이렇게 4개 검색할 수 있도록 할 예정입니다.

--- server/src/main/java/com/web6/server/controller/MovieController.java
+++ server/src/main/java/com/web6/server/controller/MovieController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -56,6 +57,81 @@ public class MovieController {
 
         ResponseVo responseVo = new ResponseVo();
         if (response.getData().isEmpty()) {
+            responseVo.setUcd("99");
+            responseVo.setMessage("영화 조회 실패");
+        } else {
+            responseVo.setUcd("00");
+            responseVo.setMessage(response.toString());
+        }
+        return ResponseEntity.ok(responseVo);
+    }
+
+
+    /* 검색 기능 */
+    @GetMapping("/movie/search")
+    public ResponseEntity<ResponseVo> searchMovies(@RequestParam(required = false) String query,
+                                                   @RequestParam(required = false) String title,
+                                                   @RequestParam(required = false) String genre,
+                                                   @RequestParam(required = false) String actor,
+                                                   @RequestParam(required = false) String director,
+                                                   @RequestParam(required = false) String nation,
+                                                   @RequestParam(required = false) String keyword) {
+
+        // 검색어를 MovieRequestVo에 설정
+        MovieRequestVo movieRequestVo = new MovieRequestVo();
+        movieRequestVo.setServiceKey("MZ6960ZIAJY0W0XX7IX7");
+        movieRequestVo.setDetail("N");
+        movieRequestVo.setListCount(50);
+
+        if (query != null) {
+            movieRequestVo.setQuery(query);
+        }
+
+        if (title != null) {
+            movieRequestVo.setTitle(title);
+        }
+
+        if (genre != null) {
+            movieRequestVo.setGenre(genre);
+        }
+
+        if (actor != null) {
+            movieRequestVo.setActor(actor);
+        }
+
+        if (director != null) {
+            movieRequestVo.setDirector(director);
+        }
+
+        if (nation != null) {
+            movieRequestVo.setNation(nation);
+        }
+
+        if (keyword != null) {
+            movieRequestVo.setKeyword(keyword);
+        }
+
+        // 영화 정보를 가져오는 서비스 호출
+        String movieResponse = movieService.getMovieInfoList(movieRequestVo);
+        //log.info("json String response : " + movieResponse);
+
+        // JSON을 MovieResponseVo 객체로 변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        MovieResponseVo response = null;
+        try {
+            response = objectMapper.readValue(movieResponse, MovieResponseVo.class);
+            log.info("MovieResponseVo : " + response);
+        } catch (JsonMappingException e) {
+            log.error("Error occurred while mapping JSON to object: " + e.getMessage(), e);
+        } catch (JsonProcessingException e) {
+            log.error("Error occurred while processing JSON: " + e.getMessage(), e);
+        } catch (Exception e) {
+            log.error("Unexpected error occurred: " + e.getMessage(), e);
+        }
+
+        // 응답 생성
+        ResponseVo responseVo = new ResponseVo();
+        if (response == null || response.getData().isEmpty()) {
             responseVo.setUcd("99");
             responseVo.setMessage("영화 조회 실패");
         } else {
