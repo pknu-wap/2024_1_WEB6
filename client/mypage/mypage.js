7c61c27868ffbfcd77f1d5cef036a71c7500b659
yeeeon02 <yeonn128902@naver.com>
2024-05-21 20:24:58 +0900

    [feature] 마이페이지 구현
    회원 정보 수정

--- client/mypage/mypage.js
+++ client/mypage/mypage.js
@@ -1,42 +1,19 @@
-// 클릭 이벤트 핸들러 등록
-var elements = document.getElementsByClassName('web-title');
-
-// 타이틀 클릭 시 메인 페이지로 이동
-elements[0].onclick = function () {
-    window.location.href = 'https://www.google.com';
+var title = document.getElementsByClassName('web-title');
+title[0].onclick = function () {
+    window.location.href = '/main_page/main.html';
 };
 
-// 프로필 수정 페이지로 이동
-function editProfile() {
-    var editProfileUrl = "https://www.naver.com/";
-    window.open(editProfileUrl, "_blank");
+
+
+function showValidation() {
+    let nickname = document.getElementById('nickname').value;
+    let newPassword = document.getElementById('newPassword').value;
+    if (nickname == "") {
+        alert("공백 또는 입력하지 않은 부분이 있습니다.");
+    }
+    else if(newPassword == ""){
+        alert("공백 또는 입력하지 않은 부분이 있습니다.");
+    }
 }
-
-
-
-//게시글 데이터
-const reviewList = [{}, {}, ... {}]; 
-const page = document.querySelector("div.page");
-
-//게시글의 총 개수   
-const totalPageList = pageList.length; 
-
-//slice()를 이용해 데이터의 전체 범위를 선택
-pageList.slice(0, totlaPageList)
-
-//map을 이용해 선택한 범위의 데이터를 조회
-	.map((e, i) => {
-  		const lists = document.createElement("div");
-        const name = document.createElement("span");
-        const content = document.createElement("p");
-        const date = document.createElement("span");
-        page.append(lists);
-        lists.append(name);
-        lists.append(date);
-        lists.append(content);
-        name.innerText = e.name;
-        date.inneerText = e.date;
-        content.innerText = e.content;
-	});
 
 
