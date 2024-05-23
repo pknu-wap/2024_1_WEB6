aba50d39123dc099db60dc6badb9e3f3db0b16bc
김민아 <alsdk__90@naver.com>
2024-05-24 07:03:39 +0900

    [feature] reviewDTO와 jsonResponse용 객체인 ApiResponse
    - ReviewDTO로 유효성 검사를 진행함
    
    - APIResponse는 프론트의 요청에 대한 응답용 객체임
    - success 필드로 요청에 대한 성공/실패를 반환
    - messag 필드로 성공/실패에 대한 메세지 반환
    - data로 관련 객체 전달
    (예를 들어, ApiResponse<ReviewDTO>처럼 쓸 수 있음. 물론 ApiResponse<Void>도 가능함.)

---
+++ server/src/main/java/com/web6/server/dto/ApiResponse.java
@@ -0,0 +1,37 @@
+package com.web6.server.dto;
+
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public ApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
