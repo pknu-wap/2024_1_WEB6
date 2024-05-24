da89dd35d0d5c65200494acb3769de12d90d2611
김민아 <alsdk__90@naver.com>
2024-05-24 16:28:36 +0900

    [feature] error 메세지 필드 추가

--- server/src/main/java/com/web6/server/dto/ApiResponse.java
+++ server/src/main/java/com/web6/server/dto/ApiResponse.java
@@ -1,13 +1,28 @@
 package com.web6.server.dto;
 
+import java.util.Map;
+
 public class ApiResponse<T> {
     private boolean success;
     private String message;
+    private Map<String, String> errors;
     private T data;
+
+    public ApiResponse() {
+
+    }
 
     public ApiResponse(boolean success, String message, T data) {
         this.success = success;
         this.message = message;
+        this.errors = null;
+        this.data = data;
+    }
+
+    public ApiResponse(boolean success, String message, Map<String, String> errors, T data) {
+        this.success = success;
+        this.message = message;
+        this.errors = errors;
         this.data = data;
     }
 
@@ -25,6 +40,14 @@ public class ApiResponse<T> {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Map<String, String> errors) {
+        this.errors = errors;
     }
 
     public T getData() {
