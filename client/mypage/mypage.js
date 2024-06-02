41a04b98b2dece5941be4a58dca672f1be51a45e
yeeeon02 <yeonn128902@naver.com>
2024-06-03 00:12:19 +0900

    [feature] 마이페이지 수정
    - 회원탈퇴 버튼 추가

--- client/mypage/mypage.js
+++ client/mypage/mypage.js
@@ -1,42 +1,164 @@
-// 클릭 이벤트 핸들러 등록
-var elements = document.getElementsByClassName('web-title');
-
-// 타이틀 클릭 시 메인 페이지로 이동
-elements[0].onclick = function () {
-    window.location.href = 'https://www.google.com';
-};
-
-// 프로필 수정 페이지로 이동
-function editProfile() {
-    var editProfileUrl = "https://www.naver.com/";
-    window.open(editProfileUrl, "_blank");
+const mainPageButton = document.querySelector('.web-title');
+if (mainPageButton) {
+    mainPageButton.addEventListener('click', () => {
+        window.location.href = '/main_page/index.html';
+    });
 }
 
+document.getElementById('logout-button').addEventListener('click', async () => {
+    try {
+        const logoutResponse = await fetch('https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/api/members/logout-page', {
+            method: 'GET',
+            credentials: 'include' // 쿠키를 포함하여 요청
+        });
+
+        if (logoutResponse.ok) {
+            window.location.href = '/main_page/index.html';
+        } else {
+            alert('로그아웃에 실패했습니다. 다시 시도해 주세요.');
+        }
+    } catch (error) {
+        console.error('Error logging out:', error);
+        alert('서버에 연결하지 못했습니다.');
+    }
+});
 
 
-//게시글 데이터
-const reviewList = [{}, {}, ... {}]; 
-const page = document.querySelector("div.page");
+// 마이페이지 정보 로드
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const response = await fetch('https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/mypage', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-//게시글의 총 개수   
-const totalPageList = pageList.length; 
+        if (response.success) {
+            const data = await response.json();
+            document.getElementById('user-id').value = data.loginId;
+            document.getElementById('nickname').value = data.nickname;
+            console.log(data.message)
+            // 다른 필요한 필드들도 여기에 추가
+        } else {
+            console.error('Failed to load user information');
+        }
+    } catch (error) {
+        console.error('서버 오류입니다.', error);
+    }
+});
 
-//slice()를 이용해 데이터의 전체 범위를 선택
-pageList.slice(0, totlaPageList)
+// 닉네임 중복 검사
+document.getElementById('check-nickname').addEventListener('click', async () => {
+    const loginId = document.getElementById('user-id').value;
+    const nickname = document.getElementById('nickname').value;
+    const currentPassword = "";
+    const newPassword = "";
+    const confirmPassword = "";
 
-//map을 이용해 선택한 범위의 데이터를 조회
-	.map((e, i) => {
-  		const lists = document.createElement("div");
-        const name = document.createElement("span");
-        const content = document.createElement("p");
-        const date = document.createElement("span");
-        page.append(lists);
-        lists.append(name);
-        lists.append(date);
-        lists.append(content);
-        name.innerText = e.name;
-        date.inneerText = e.date;
-        content.innerText = e.content;
-	});
+    try {
+        const response = await fetch('https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app//mypage/duplicate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nickname,
+                loginId,
+                currentPassword,
+                newPassword,
+                confirmPassword
+            })
+        });
 
+        if (response.success) {
+            const data = await response.json();
+
+            if (data.success) {  // 중복 확인 성공
+                document.getElementById('nicknameChecked').value = 'true';
+                document.getElementById('nickname-error').textContent = data.message;
+            } else {  // 중복 확인 실패
+                document.getElementById('nicknameChecked').value = 'false';
+                console.log(data.message);
+
+                if (data.errors) {
+                    if (data.errors.valid_nickname) {     // 닉네임 유효성 검사 탈락
+                        document.getElementById('nickname-error').textContent = data.errors.valid_nickname;
+                    } else if (data.errors.duplicate) {    // 닉네임이 중복될 때
+                        document.getElementById('nickname-error').textContent = data.errors.duplicate;
+                    } else if (data.errors.change) {       // 닉네임에 변경사항이 없을 때
+                        document.getElementById('nickname-error').textContent = data.errors.change;
+                    } else {
+                        alert('중복 확인 실패. 다시 시도해주세요.');
+                    }
+                } else {
+                    alert('중복 확인 실패. 다시 시도해주세요.');
+                }
+            }
+        } else {
+            console.error('Response not ok:', response.statusText);
+            alert('중복 확인 실패. 다시 시도해주세요.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Error:', error);
+    }
+});
+
+// 회원 정보 수정 저장
+document.getElementById('update-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const nickname = document.getElementById('nickname').value;
+    const loginId = document.getElementById('user-id').value;
+    const currentPassword = document.getElementById('current-password').value || '';
+    const newPassword = document.getElementById('new-password').value || '';
+    const confirmPassword = document.getElementById('confirm-new-password').value || '';
+    const isNicknameChecked = document.getElementById('nicknameChecked').value === 'true';
+
+    try {
+        const response = await fetch('https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/mypage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nickname,
+                loginId,
+                currentPassword,
+                newPassword,
+                confirmPassword,
+                isNicknameChecked
+            }),
+        });
+
+        const data = await response.json();
+
+        if (response.success) {   // 수정 성공
+            if (data.success) {
+                alert(data.message);
+                window.location.href = '/main_page/index.html';
+            } else {   // 수정 실패 
+                if (!data.nicknameChecked && data.errors && data.errors.nickname) {  // 닉네임 수정 실패
+                    alert(data.message);
+                } else if (data.errors && data.errors.errorMessage) {  // 현재 비밀번호가 불일치
+                    console.log(data.message);
+                    document.getElementById('current-password-error').textContent = data.errors.errorMessage;
+                } else if (data.errors && data.errors.valid_password) {  // 새 비밀번호의 유효성 검사 탈락
+                    console.log(data.message);
+                    document.getElementById('new-password-error').textContent = data.errors.valid_password;
+                } else if (data.errors && data.errors.confirm_password) {  // 비밀번호 확인 불일치
+                    console.log(data.message);
+                    document.getElementById('confirm-new-password-error').textContent = data.errors.confirm_password;
+                }
+            }
+        } else {
+            console.error('Response not ok:', data);
+            alert('오류가 발생했습니다. 다시 시도해주세요.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('오류가 발생했습니다. 다시 시도해주세요.');
+    }
+});
 
