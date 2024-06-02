f14e459a3780b84204b85045b0b8d4deabcd242f
yeeeon02 <yeonn128902@naver.com>
2024-06-03 00:09:33 +0900

    [feature] 메인 페이지 수정
    - 로그인 상태에 따라 ui 변경
    - 로그아웃 기능 구현
    - 영화 데이터 화면에 표시

--- client/main_page/index.js
+++ client/main_page/index.js
@@ -1,106 +1,51 @@
-// 웹 페이지 이동
-var webTitle = document.getElementsByClassName('web-title');
-webTitle[0].onclick = function () {
-    window.location.href = '/main_page/index.html';
-};
-
-var loginButton = document.getElementsByClassName('login-button');
-loginButton[0].onclick = function () {
-    window.location.href = '/login/login-page.html';
-};
-
-var signupButton = document.getElementsByClassName('signup-button');
-signupButton[0].onclick = function () {
-    window.location.href = '/join_page/join_page2.html';
-};
-
-// 검색
 document.addEventListener('DOMContentLoaded', () => {
-    const searchForm = document.getElementById('search-form');
-    const searchInput = document.getElementById('search-input');
-    const searchOption = document.getElementById('search-option');
-
-    searchForm.addEventListener('submit', (event) => {
-        event.preventDefault(); // 폼 제출 기본 동작을 막음
-
-        // 검색어 옵션 가져오기
-        const query = searchInput.value.trim();
-        const option = searchOption.value;
-
-        if (query) {
-            // 검색어가 있는 경우
-            window.location.href = `https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/movies/search/json-tag?option=${option}&query=${encodeURIComponent(query)}`;
-        } else {
-            // 검색어가 공백인 경우
-            window.location.href = 'https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/movies/search/json-tag';
-        }
-    });
-});
-
-// 게시글 클릭
-function handleClick(movieSeq) {
-    const apiUrl = `https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/movies/detail/${movieSeq}`;
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ movieSeq }), // movieSeq를 JSON 형식으로 변환하여 서버로 전송
+    // 웹 페이지 이동
+    document.querySelector('.web-title').onclick = () => {
+        window.location.href = '/main_page/index.html';
     };
 
-    fetch(apiUrl, options)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("Movie detail response:", data); // 서버에서 받은 데이터를 콘솔에 출력
-            // 받은 데이터를 활용하여 상세 정보를 표시하거나 다른 작업 수행
-        })
-        .catch((error) => {
-            console.error("Error fetching movie detail:", error);
-        });
-}
+    document.querySelector('.login-button').onclick = () => {
+        window.location.href = '/login/login-page.html';
+    };
 
-// 메인페이지 영화 목록 표시
-document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.signup-button').onclick = () => {
+        window.location.href = '/join_page/join_page2.html';
+    };
+
+    // 게시글 클릭
+    function handleClick(movieSeq) {
+        const apiUrl = `https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/movies/detail/${movieSeq}`;
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Movie detail response:", data);
+                // 받은 데이터를 활용하여 상세 정보를 표시하거나 다른 작업 수행
+            })
+            .catch((error) => {
+                console.error("Error fetching movie detail:", error);
+            });
+    }
+
+    // 메인페이지 영화 목록 표시
     const sortOption = document.getElementById('sort-option');
-    const searchForm = document.getElementById('search-form');
-    const searchInput = document.getElementById('search-input');
-    const searchOption = document.getElementById('search-option');
     const prevPageButton = document.getElementById('prevPage');
     const nextPageButton = document.getElementById('nextPage');
     const pageInfo = document.getElementById('pageInfo');
 
-
-    let currentPage = 1;   // 현재 페이지
-    const moviesPerPage = 10;   // 한 페이지에 표시할 영화 개수
+    let currentPage = 1; // 현재 페이지
+    const moviesPerPage = 10; // 한 페이지에 표시할 영화 개수
 
     // 페이지 로드 시 최신순으로 영화 가져오기
     fetchMovies('latest', currentPage);
 
-    // 정렬 옵션이 변경될 때 이벤트 리스너 추가
+    // 정렬 옵션 변경 이벤트 리스너
     sortOption.addEventListener('change', (event) => {
         const selectedOption = event.target.value;
         currentPage = 1;
         fetchMovies(selectedOption, currentPage);
     });
 
-    // 검색 폼 제출 이벤트 리스너 추가
-    searchForm.addEventListener('submit', (event) => {
-        event.preventDefault(); // 폼 제출 기본 동작을 막음
-
-        // 검색어 옵션 가져오기
-        const query = searchInput.value.trim();
-        const option = searchOption.value;
-
-        if (query) {
-            // 검색어가 있는 경우
-            window.location.href = `https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/movies/search/json-tag?option=${option}&query=${encodeURIComponent(query)}`;
-        } else {
-            // 검색어가 공백인 경우
-            window.location.href = 'https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/movies/findAll';
-        }
-    });
-
-    // 페이지네이션 버튼 이벤트 리스너 추가
+    // 페이지네이션 버튼 이벤트 리스너
     prevPageButton.addEventListener('click', () => {
         if (currentPage > 1) {
             currentPage--;
@@ -113,64 +58,59 @@ document.addEventListener('DOMContentLoaded', () => {
         fetchMovies(sortOption.value, currentPage);
     });
 
-
-
     // 영화 데이터 가져오기
     function fetchMovies(orderBy, page) {
-        // 더미 데이터
-        const dummyData = {
-            "movies": [
-                {
-                    // "movieSeq": 1,
-                    // "title": "Movie Title 1",
-                    // "prodYear": "2021",
-                    // "posterUrl": "https://via.placeholder.com/200x300",
-                    // "rating": 4
-                },
-
-            ],
-            "totalPages": 1
-        };
-        const data = dummyData; // 더미 데이터를 사용합니다.
-        // 데이터를 화면에 표시합니다.
-        displayMovies(data.movies);
-        updatePageInfo(data.totalPages);
-
-
-
-
         let apiUrl;
+
         // 정렬 옵션에 따라 API URL 설정
         if (orderBy === 'latest') {
-            // apiUrl = `https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/movie/latest?page=${page}&size=${moviesPerPage}`;
+            apiUrl = `https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/movie/latest`;
         } else if (orderBy === 'comments') {
-            apiUrl = `https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/Top5OrderByGradeCountDesc?page=${page}&size=${moviesPerPage}`;
+            apiUrl = `https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/Top5OrderByGradeCountDesc`;
         }
 
         // API 요청
         fetch(apiUrl)
             .then(response => response.json())
             .then(data => {
-                displayMovies(data.movies);
-                updatePageInfo(data.totalPages);
+                if (data.status === 500) {
+                    alert(data.message);
+                } else {
+                    if (orderBy === 'latest') {
+                        displayLatestMovies(data);
+                    } else if (orderBy === 'comments') {
+                        displayCommentedMovies(data);
+                    }
+                    updatePageInfo(currentPage, data.totalPages);
+                }
             })
-            .catch(error => console.error("Error fetching movies:", error));
+            .catch(error => {
+                console.error("Error fetching movies:", error);
+                alert("영화 데이터를 불러오는 중 오류가 발생했습니다.");
+            });
+
     }
 
-    // 영화 데이터를 화면에 표시
-    function displayMovies(movies) {
+    // 최신순 영화 데이터를 화면에 표시
+    function displayLatestMovies(movies) {
         const moviesGrid = document.getElementById("moviesGrid");
         moviesGrid.innerHTML = "";
 
+        const dummyImageUrl = 'https://via.placeholder.com/200x300'; // 더미 이미지 URL
+
         movies.forEach(movie => {
             const movieCard = document.createElement("div");
-            movieCard.classList.add("movie-card");  // 영화 카드 스타일
+            movieCard.classList.add("movie-card");
+
+            // 포스터 이미지 가져오기, 없으면 더미 이미지 사용
+            const posterUrl = (movie.postersList && movie.postersList.length > 0) ? movie.postersList[0] : dummyImageUrl;
+
             movieCard.innerHTML = `
-            <img src="${movie.posterUrl}" alt="${movie.title}">
-            <div class="movie-title">${movie.title} (${movie.commentCount})</div>
-            <div class="movie-rating" style="margin-top: -10px">★ ${movie.rating}</div>
+                <img src="${posterUrl}" alt="${movie.title}" class="movie-poster" data-movie-id="${movie.movieSeq}">
+                <div class="movie-title" data-movie-id="${movie.movieSeq}">${movie.title}</div>
+                <div class="movie-rating" style="margin-top: -10px">★ ${movie.rating}</div>
             `;
-            moviesGrid.appendChild(movieCard);   // 영화 카드를 영화 목록에 추가
+            moviesGrid.appendChild(movieCard);
         });
 
         const movieElements = document.querySelectorAll('.movie-poster, .movie-title');
@@ -182,75 +122,36 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    //페이지 정보를 업데이트하는 함수
-    function updatePageInfo(totalPages) {
-        pageInfo.textContent = `Page ${currentPage} of ${totalPages}`;
-        prevPageButton.disabled = currentPage === 1;
-        nextPageButton.disabled = currentPage === totalPages;
-    }
-});
+    // 댓글순 영화 데이터를 화면에 표시
+    function displayCommentedMovies(movies) {
+        const moviesGrid = document.getElementById("moviesGrid");
+        moviesGrid.innerHTML = "";
 
+        movies.forEach(movie => {
+            const movieCard = document.createElement("div");
+            movieCard.classList.add("movie-card");
 
+            movieCard.innerHTML = `
+                <img src="${posterUrl}" alt="${movie.title}" class="movie-poster" data-movie-id="${movie.movieSeq}">
+                <div class="movie-title" data-movie-id="${movie.movieSeq}">${movie.title}</div>
+                <div class="movie-rating" style="margin-top: -10px">★ ${movie.rating}</div>
+            `;
+            moviesGrid.appendChild(movieCard);
+        });
 
-// 로그인 상태 확인
-function checkLoginStatus() {
-    const loginSection = document.querySelector('.login-section');
-    const nickname = sessionStorage.getItem('nickname');
-
-    if (nickname) {
-        loginSection.innerHTML = `<span>${nickname}님 환영합니다.</span>
-                                  <button id="logout-button">로그아웃</button>`;
-
-        document.getElementById('logout-button').addEventListener('click', function () {
-            sessionStorage.removeItem('nickname');
-            loginSection.innerHTML = `<button class="login-button" id="login-button">로그인</button>
-                                      <button class="signup-button">회원가입</button>`;
-            document.getElementById('login-button').addEventListener('click', function () {
-                window.location.href = '/login/login-page.html';
+        const movieElements = document.querySelectorAll('.movie-title');
+        movieElements.forEach(element => {
+            element.addEventListener('click', () => {
+                const movieId = element.getAttribute('data-movie-id');
+                handleClick(movieId);
             });
         });
     }
-}
 
-document.getElementById('login-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const username = document.getElementById('username').value.trim();
-    const password = document.getElementById('password').value.trim();
-
-    if (username === '' || password === '') {
-        alert('아이디와 비밀번호를 모두 입력해주세요.');
-        return;
-    }
-
-    const formData = new URLSearchParams();
-    formData.append('username', username);
-    formData.append('password', password);
-
-    try {
-        const response = await fetch('https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/api/members/login-page', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            credentials: 'include',
-            body: formData.toString()
-        });
-
-        const data = await response.json();
-        console.log(data);
-        let messageElement = document.getElementById("messageElement");
-        messageElement.textContent = data.message;
-
-        if (response.ok && data.message === '로그인 성공') {
-            alert(data.message);
-            sessionStorage.setItem('nickname', data.nickname); // 서버가 닉네임을 반환한다고 가정
-            window.location.href = '/main_page/index.html';
-        } else {
-            alert(data.message);
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        alert('오류가 발생했습니다. 다시 시도해주세요.');
+    // 페이지 정보를 업데이트하는 함수
+    function updatePageInfo(currentPage, totalPages) {
+        pageInfo.textContent = `Page ${currentPage} of ${totalPages}`;
+        prevPageButton.disabled = currentPage === 1;
+        nextPageButton.disabled = currentPage === totalPages;
     }
 });
---
+++ client/main_page/main_auth.js
@@ -0,0 +1,60 @@
+// 로그인 상태 확인
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        // 요청
+        const response = await fetch('https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/api/main', {
+            method: 'GET',
+            credentials: 'include' // 쿠키를 포함하여 요청
+        });
+
+        const data = await response.json();
+        console.log('Session Data:', data);
+
+        // UI 요소 참조
+        const loginSection = document.getElementById('login-section');
+        const userInfoSection = document.getElementById('user-info-section');
+        const welcomeMessage = document.getElementById('welcome-message');
+
+        // 응답 처리
+        if (data.success && data.message !== '로그인을 해주세요.') {  // 로그인된 상태
+            // UI 업데이트
+            welcomeMessage.innerHTML = `<a href="/myreviews/${encodeURIComponent(data.message)}">${data.message}</a>! 환영합니다.`;
+            loginSection.style.display = 'none';
+            userInfoSection.style.display = 'block';
+
+            document.querySelector('#welcome-message a').addEventListener('click', (event) => {
+                event.preventDefault();
+                window.location.href = `/myreview/${encodeURIComponent(data.message)}`;
+            });
+
+            // 로그아웃 버튼 동작 설정
+            document.getElementById('logout-button').addEventListener('click', async () => {
+                try {
+                    const logoutResponse = await fetch('https://port-0-web6-1pgyr2mlvnqjxex.sel5.cloudtype.app/api/members/logout-page', {
+                        method: 'GET',
+                        credentials: 'include' // 쿠키를 포함하여 요청
+                    });
+
+                    if (logoutResponse.ok) {
+                        window.location.href = '/main_page/index.html';
+                    } else {
+                        alert('로그아웃에 실패했습니다. 다시 시도해 주세요.');
+                    }
+                } catch (error) {
+                    console.error('Error logging out:', error);
+                    alert('서버에 연결하지 못했습니다.');
+                }
+            });
+
+        } else {
+            // 로그인되지 않은 상태
+            console.log(data.message);
+            // loginSection.style.display = 'block';
+            userInfoSection.style.display = 'none';
+        }
+
+    } catch (error) {
+        console.error('서버 에러:', error);
+    }
+});
+
