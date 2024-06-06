afa5afe2eb93dea4993dcaacc4bda5af054cd79e
강홍구 <ghdrn1399@naver.com>
2024-06-06 23:11:42 +0900

    [conflict] SecurityConfig 충돌 해결

--- server/src/main/java/com/web6/server/config/SecurityConfig.java
+++ server/src/main/java/com/web6/server/config/SecurityConfig.java
@@ -112,12 +112,8 @@ public class SecurityConfig {
     @Bean
     public CookieSerializer cookieSerializer() {
         DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-<<<<<<< HEAD
-        serializer.setSameSite("Lax"); // SameSite 설정을 Lax로 설정
-=======
         serializer.setSameSite("None"); // SameSite 설정을 None으로 설정
         serializer.setUseSecureCookie(true); // Secure 속성을 설정 (HTTPS에서만 작동)
->>>>>>> 5c1c193fcf747b9093a767d7ac0899f7ccc20518
         return serializer;
     }
 }
